c16c3e8e87cbd4d381a4d1f98350104b70bdab48
C Banuelos <holomorfo.mx@gmail.com>
2023-04-06 19:36:25 -0700

    example passing text

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,9 +1,11 @@
 const sum = require('../library.js')
 
 test('adds 1 + 2 to equal 3', () => {
+    // Ejemplo de prueba exitosa
   expect(sum(1, 2)).toBe(3)
 })
 
-test('adds 1 + 3 to equal 4', () => {
-  expect(sum(1, 3)).toBe(3)
-})
+// test('adds 1 + 3 to equal 4', () => {
+//     // Ejemplo de prueba que falla
+//   expect(sum(1, 3)).toBe(3)
+// })
