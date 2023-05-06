880b38f6025c96e78154b9ad6c8b0223cd23ab08
hvillase <hernani.vr@gmail.com>
2023-05-06 11:38:43 -0600

    traducción acerca inglés

--- src/traducciones/strings.js
+++ src/traducciones/strings.js
@@ -33,7 +33,7 @@ const sp = {
       musical lo que hace de este desarrollo un campo interdisciplinario para construir una tecnología de sonido
       aplicada a la web.
 
-      La idea surge como respuesta a llevar un semestre dedicado al desrrollo de tecnologías basadas en la programación
+      La idea surge como respuesta a llevar un semestre dedicado al desarrollo de tecnologías basadas en la programación
       de manera colectiva y guiada bajo un flujo de trabajo propio del desarrollo web.
 
       De esta manera el quehacer artístico de sus integrantes encuentra una metodología de desarrollo que integra
@@ -41,7 +41,7 @@ const sp = {
 }
 
 const en = {
-  tituloproyecto: 'the TITULO DEL PROYECTO',
+  tituloproyecto: 'PROJECT TITLE',
   link_inicio: 'inicio',
   link_sonido: 'sonido',
   link_acerca: 'acerca',
@@ -70,19 +70,13 @@ const en = {
     'Ejemplo para relacionar carga y reproducción de archivos locales.',
   header_editor: 'Editor sencillo',
   header_acerca: 'acerca de nosotros',
-  desc_acerca: `El equipo de desarrollo e investigación de esta app forma parte del Seminario Permanente de Tecnología Musical
-      del Posgrado en Música UNAM. Sus integrantes tienen diferentes perfiles e intereses alrededor de la tecnología
-      musical lo que hace de este desarrollo un campo interdisciplinario para construir una tecnología de sonido
-      aplicada a la web.
+  desc_acerca: `This app's development and research team is part of the Permanent Seminar of Music Technology of the Postgraduate Program in Music UNAM. Its members have different profiles and interests around music technology, which makes this development an interdisciplinary field to build a sound technology applied to the web.
+  The idea arises as a response to a semester dedicated to developing technologies based on programming in a collective and guided way under a web development workflow.
+  In this way, the artistic work of its members finds a development methodology that integrates questions from the musical practice.`
 
-      La idea surge como respuesta a llevar un semestre dedicado al desrrollo de tecnologías basadas en la programación
-      de manera colectiva y guiada bajo un flujo de trabajo propio del desarrollo web.
-
-      De esta manera el quehacer artístico de sus integrantes encuentra una metodología de desarrollo que integra
-      preguntas que vienen de la práctica musical.`
 }
 const fr = {
-  tituloproyecto: 'le TITULO DEL PROYECTO',
+  tituloproyecto: 'TITRE DU PROJET',
   link_inicio: 'inicio',
   link_sonido: 'sonido',
   link_acerca: 'acerca',
