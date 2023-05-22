00fc9dad6012e16f6ee48cafc3f0b3bf1e4d2d83
C Banuelos <holomorfo.mx@gmail.com>
2023-05-21 20:52:50 -0700

    Agregar boton para play y stop linea actual

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { EditorParser } from './editorCodeMirror/editorParser'
 import { SPTMController } from './sptm-live/SPTMController'
 
+let parent = document.querySelector('#editor')
+let stopSoundObj = document.querySelector('#detenerSeq')
+let playLineObj = document.querySelector('#playLine')
+
 var AudioContext = window.AudioContext || window.webkitAudioContext
 const audioCtx = new AudioContext()
 
 let { parser } = SPTMController(audioCtx)
+let { playCurrentLine, stopLine } = new EditorParser({ parent, parser })
 
-let parent = document.querySelector('#editor')
-let editor = new EditorParser({ parent, parser })
+stopSoundObj.addEventListener('click', () => stopLine())
+playLineObj.addEventListener('click', () => playCurrentLine())
